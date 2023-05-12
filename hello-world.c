c80500fec8b1a4e055e39c9041deb0a3e3c7b293
man-chro96 <manolis.chro@gmail.com>
2023-05-12 16:31:12 +0300

    Fixed a problem with printf

--- hello-world.c
+++ hello-world.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-int main() {
+int main()
+{
    // printf() displays the string inside quotation
    printf("Hello, World!\n");
-   printf("Hello obs\n");
+   // printf("Hello obs\n");
    printf("Hello jenkins\n");
    return 0;
 }
