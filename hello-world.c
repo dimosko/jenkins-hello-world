4ee0487ccc16d1977b8b595648e91d849663b9d3
Konstantinos Dimos <dimosko4@gmail.com>
2023-05-12 16:15:16 +0300

    second_commit

--- hello-world.c
+++ hello-world.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, World!");
-   printf("Hello obs");
+   printf("Hello, World!\n");
+   printf("Hello obs\n");
+   printf("Hello jenkins\n");
    return 0;
 }
