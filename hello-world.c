cef0c89fa9967a647531e3787035169d69c36bce
man-chro96 <manolis.chro@gmail.com>
2023-05-15 14:57:15 +0300

    post commit hook test

--- hello-world.c
+++ hello-world.c
@@ -2,7 +2,7 @@
 int main()
 {
    // printf() displays the string inside quotation
-   printf("Hello, iSekai!\n");
+   printf("Hello, iiiSekai!\n");
    // printf("Hello obs\n");
    printf("Hello jenkins\n");
    return 0;
