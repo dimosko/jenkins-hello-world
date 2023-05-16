eb17e289f23eb1b3f8c827343b9f5559dee4014b
man-chro96 <manolis.chro@gmail.com>
2023-05-16 09:46:53 +0300

    hello from test

--- hello-world.c
+++ hello-world.c
@@ -4,6 +4,6 @@ int main()
    // printf() displays the string inside quotation
    printf("Hello, za Warudo! Muda\n");
    // printf("Hello obs\n");
-   printf("Hello jenkins\n");
+   printf("Hello from test branch\n");
    return 0;
 }
